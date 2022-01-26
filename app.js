040426a9329065f639dc47d59517c5a10d6720f4
Rbazan22 <73728631+Rbazan22@users.noreply.github.com>
2022-01-25 19:45:03 -0600

    Change JS File to be more DRY

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ const newList = [
 
 const cssClasses = {
   pending:
-    "bg-white w-full text-center text-red-500 rounded py-4 border-2 border-red-500 transition transform ease-in-out duration-300 hover:bg-red-500 hover:text-white hover:scale-110 hover:rotate-1 cursor-pointer",
-  done: "bg-white w-full text-center text-green-500 rounded py-4 border-2 border-green-500 transition transform ease-in-out duration-300 hover:bg-green-500 hover:text-white hover:scale-110 hover:-rotate-1 cursor-pointer",
+    "bg-white w-full text-center text-blue-500 rounded py-4 border-2 border-blue-500 transition transform ease-in-out duration-300 hover:bg-blue-500 hover:text-white hover:scale-110 hover:rotate-1 cursor-pointer",
+  done: "bg-white w-full text-center text-purple-500 rounded py-4 border-2 border-purple-500 transition transform ease-in-out duration-300 hover:bg-purple-500 hover:text-white hover:scale-110 hover:-rotate-1 cursor-pointer",
 };
 
 const updateTodos = () => {
